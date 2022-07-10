89a7de0dae59feb6ca7a6ff9fe0d403493b23450
mkchauhan647 <mkchauhan647@gmail.com>
2022-07-11 01:33:51 +0545

    Initial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mroot/vscode/tensor
+# Build directory: /home/mroot/vscode/tensor/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
